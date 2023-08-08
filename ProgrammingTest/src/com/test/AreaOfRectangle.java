96173bdfd7b741650506f4d6aad5492d1f02cc0e
Anupam <Anupamkaphen@gmail.com>
2023-08-08 21:52:52 +0530

    first commit

---
+++ ProgrammingTest/src/com/test/AreaOfRectangle.java
@@ -0,0 +1,13 @@
+package com.test;
+
+public class AreaOfRectangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int w=5,h=10;
+		int area=w*h;
+		System.out.println("Area of rectangle =" +area);
+	}
+
+}
