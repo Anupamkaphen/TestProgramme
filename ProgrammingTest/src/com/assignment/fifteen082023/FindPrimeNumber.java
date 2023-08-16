be7051afad819cf9be762aa400f67eba312dab3c
Anupam <Anupamkaphen@gmail.com>
2023-08-17 00:00:29 +0530

    Prime no  programme

---
+++ ProgrammingTest/src/com/assignment/fifteen082023/FindPrimeNumber.java
@@ -0,0 +1,19 @@
+package com.assignment.fifteen082023;
+
+public class FindPrimeNumber {
+
+	 public static void main(String args[]) {
+	        for (int i = 100; i <= 200; i++) {
+	            int c = 0;
+	            for (int j = 1; j <= i; j++) {
+	                if (i % j == 0)
+	                    c++;
+	            }
+	            if (c == 2)
+	                System.out.println(i);
+	        }
+	    }
+
+	}
+
+
